87302b74a03c0ce10142e15100f15f14197d3edf
Misha <m.krovyakov@bk.ru>
2022-04-06 09:02:09 +0300

    L4 complite

--- PingPong/Source/PingPong/APingPongBall.cpp
+++ PingPong/Source/PingPong/APingPongBall.cpp
@@ -1,27 +1,145 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "APingPongBall.h"
-
+#include "DrawDebugHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Net/UnrealNetwork.h"
 // Sets default values
-AAPingPongBall::AAPingPongBall()
+APingPongBall::APingPongBall()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+// Set this actor to call Tick() every frame. You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-}
-
-// Called when the game starts or when spawned
-void AAPingPongBall::BeginPlay()
-{
-	Super::BeginPlay();
+	BodyCollision = CreateDefaultSubobject<USphereComponent>(TEXT("Ball BodyCollider"));
+	SetRootComponent(BodyCollision);
 	
+	BodyMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Ball Body Mesh"));
+	BodyMesh->SetupAttachment(RootComponent);
+	BodyMesh->SetIsReplicated(true);
+	BodyMesh->OnComponentBeginOverlap.AddDynamic(this, &APingPongBall::OnMeshOverlapBegin);
+	
+	SetReplicates(true);
+	SetReplicateMovement(true);
 }
-
-// Called every frame
-void AAPingPongBall::Tick(float DeltaTime)
+// Called when the game starts or when spawned
+void APingPongBall::BeginPlay()
 {
-	Super::Tick(DeltaTime);
+Super::BeginPlay();
+}
+// Called every frame
+void APingPongBall::Tick(float DeltaTime)
+{
+Super::Tick(DeltaTime);
 
+	
+
+	if (GetNetMode() != ENetMode::NM_Client)
+	{
+		Server_Move(DeltaTime);
+	}
 }
 
+////////////////////////////// Lesson 4 //////////////////////////////////////////////
+void APingPongBall::OnMeshOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor*
+OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool
+bFromSweep, const FHitResult& SweepResult)
+{
+	
+	UE_LOG(LogTemp, Error, TEXT(" %s collided with %s. "), *GetName(),	*OtherActor->GetName());
+}
+////////////////////////////// Lesson 4 //////////////////////////////////////////////
+void APingPongBall::StartMove()
+{
+Server_StartMove();
+}
+void APingPongBall::StopMove()
+{
+Server_StopMove();
+}
+void APingPongBall::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>&
+OutLifetimeProps) const
+{
+Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+DOREPLIFETIME(APingPongBall, IsMoving);
+}
+bool APingPongBall::Server_Move_Validate(float DeltaTime)
+{
+return true;
+}
+void APingPongBall::Server_Move_Implementation(float DeltaTime)
+{
+FVector forward = GetActorForwardVector();	
+FVector currLoc = GetActorLocation();
+FVector newLoc = currLoc + forward * MoveSpeed * DeltaTime;
+FHitResult hitResult;
+	coordinatZ = currLoc.Z;
+
+//	UE_LOG(LogTemp, Display, TEXT("APingPongBall::Server_Move_Implementation"));
+	
+if(!SetActorLocation(newLoc, true, &hitResult))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Ball %s Collided with %s"), *GetName(),        *hitResult.GetActor()->GetName());
+	
+        FVector moveVector = forward - currLoc;
+	
+        moveVector.Normalize();
+	
+        FVector resetPosition = currLoc + moveVector * DeltaTime * 5 * MoveSpeed;
+	
+        DrawDebugDirectionalArrow(GetWorld(), newLoc + moveVector * 300, newLoc, 30, FColor::Green, true, 3.f, 0, 3);
+	
+        FVector impactCorrection = hitResult.ImpactPoint + hitResult.ImpactNormal * 300;
+	
+        DrawDebugDirectionalArrow(GetWorld(), hitResult.ImpactPoint, hitResult.ImpactPoint + hitResult.ImpactNormal
+        	* 300, 30, FColor::Blue, true, 3, 0, 3);
+	
+        float AimAtAngle = FMath::RadiansToDegrees(acosf(FVector::DotProduct(moveVector, hitResult.ImpactNormal)));
+	
+        moveVector = moveVector.RotateAngleAxis(AimAtAngle*2, FVector(0,0,1));
+	
+        FVector newTargetMove = newLoc + moveVector * 300;
+	
+        newTargetMove.Z = currLoc.Z;
+		resetPosition.Z = coordinatZ;
+	
+        DrawDebugDirectionalArrow(GetWorld(), newLoc, newTargetMove, 30, FColor::Yellow, true, 3.f, 0, 3);
+	
+        SetActorLocation(resetPosition);
+	
+        FRotator currRotation = GetActorRotation();
+	
+        FRotator newRotation = UKismetMathLibrary::FindLookAtRotation(currLoc, newTargetMove);
+	
+        newRotation.Pitch = currRotation.Pitch;
+	
+        SetActorRotation(newRotation);
+	
+        Multicast_HitEffect();
+        }
+	}
+	
+
+bool APingPongBall::Server_StartMove_Validate()
+{
+return true;
+}
+void APingPongBall::Server_StartMove_Implementation()
+{
+IsMoving = true;
+}
+bool APingPongBall::Server_StopMove_Validate()
+{
+return true;
+}
+void APingPongBall::Server_StopMove_Implementation()
+{
+IsMoving = false;
+}
+void APingPongBall::Multicast_HitEffect_Implementation()
+{
+UWorld * world = GetWorld();
+if(world && HitEffect)
+{
+UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), HitEffect,
+GetActorLocation());
+}
+
+}
--- PingPong/Source/PingPong/APingPongBall.h
+++ PingPong/Source/PingPong/APingPongBall.h
@@ -1,26 +1,48 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
-
 #include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/Actor.h"
 #include "APingPongBall.generated.h"
-
 UCLASS()
-class PINGPONG_API AAPingPongBall : public AActor
+class PINGPONG_API APingPongBall : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	AAPingPongBall();
+	protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	USphereComponent* BodyCollision;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* BodyMesh;
 
-protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ball params")
+	float MoveSpeed = 100;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Ball params")
+	UParticleSystem* HitEffect;
+	UPROPERTY(Replicated)
+	bool IsMoving = true;
+	float coordinatZ;
+	public:
+	// Sets default values for this actor's properties
+	APingPongBall();
+	protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_Move(float DeltaTime);
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_StartMove();
+	UFUNCTION(Server, Reliable, WithValidation)
+	void Server_StopMove();
+	UFUNCTION(NetMulticast, Unreliable)
+	void Multicast_HitEffect();
+	public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	UFUNCTION(BlueprintCallable)
+	void StartMove();
+	UFUNCTION(BlueprintCallable)
+	void StopMove();
+	virtual void GetLifetimeReplicatedProps(TArray< class FLifetimeProperty > &	OutLifetimeProps) const;
+	void OnMeshOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor*
+OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool
+bFromSweep, const FHitResult& SweepResult);
 };
